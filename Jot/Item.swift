104e0e67d3399c8f93384ba053ec3b7a31811b8e
Foiler25 <foiler25@gmail.com>
2026-04-19 16:30:54 -0400

    Initial Commit

---
+++ Jot/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Jot
+//
+//  Created by Brandon Villar on 4/19/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
